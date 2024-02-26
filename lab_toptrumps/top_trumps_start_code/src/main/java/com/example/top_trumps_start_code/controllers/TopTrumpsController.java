9e49ff23ae808104f12b7ca22d5c68a9189390a1
Birindar Kaur Bahia <156370888+BirindarK@users.noreply.github.com>
2024-02-26 17:01:37 +0000

    MVP and first extension completed

---
+++ lab_toptrumps/top_trumps_start_code/src/main/java/com/example/top_trumps_start_code/controllers/TopTrumpsController.java
@@ -0,0 +1,27 @@
+package com.example.top_trumps_start_code.controllers;
+
+import com.example.top_trumps_start_code.models.Card;
+import com.example.top_trumps_start_code.services.TopTrumpsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+
+@RestController
+@RequestMapping(value = "/toptrumps")
+public class TopTrumpsController {
+
+    @Autowired
+    TopTrumpsService topTrumpsService;
+
+    @PostMapping
+    public ResponseEntity<String> compareCards(@RequestBody ArrayList<Card> cards){
+        String card = topTrumpsService.checkWinner(cards);
+        return new ResponseEntity<>(card, HttpStatus.OK);
+
+    }
+
+}
